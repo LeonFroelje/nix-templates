b92059b2b0826324f0ea5a5ad5e3bfca587c7868
leon froelje <Leon.froelje@uni-bremen.de>
2024-12-27 01:06:33 +0100

    lel

--- iac/flake.nix
+++ iac/flake.nix
@@ -1,21 +1,56 @@
 {
-  description = "iac flake";
+  description = "NixOS VM configs";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sshKeys = {
+      url = "https://github.com/LeonFroelje.keys";
+      flake = false;
+    };
+    terranix = {
+      url = "github:terranix/terranix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, ... }: 
+  outputs = { self, sshKeys, nixpkgs, nixos-generators, terranix, ... }: 
   let
     system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-        packages = [
-            pkgs.opentofu
-            pkgs.nixos-generators
-        ];
-        shellHook = "zsh";
+    pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+    specialArgs = {
+      inherit sshKeys;
+      inherit pkgs;
     };
+    modules = [
+      ./modules/common.nix
+      ./modules/hardware.nix
+      {
+        nix.registry.nixpkgs.flake = nixpkgs;
+      }
+    ];
+    systems = [
+      {
+        name = "example";
+        value = {
+          inherit system;
+          modules = modules ++ [
+            # modules/example.nix
+          ];
+          inherit specialArgs;
+        };
+      }
+    ];
+    qcow = {
+      format = "qcow";
+    };
+
+  in {
+    packages.${system} = builtins.listToAttrs (map (x: { name = x.name; value = nixos-generators.nixosGenerate (x.value // qcow); }) systems);
+
+    nixosConfigurations = builtins.listToAttrs (map (x: { name = x.name; value = nixpkgs.lib.nixosSystem x.value; }) systems);    
   };
 }
